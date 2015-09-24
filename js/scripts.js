b57fb7e85f3873d3eb36fcc5efe2c4e10f5db0e0
Summer <sbrochtrup@gmail.com>
2015-09-23 21:04:21 -0700

    Add reactionTime results to HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
 	var clickedTime;
 	var newBoxTime;
-	var reationTime;
+	var reactionTime;
 	Date.now();
 	function newBox() {
-		newBoxTime = Date.now();
 		var time = Math.random() * 5000;
 		setTimeout(function() {
 			document.getElementById("box").style.display="block";
+			newBoxTime = Date.now();
 		}, time);
 	}
 
 	document.getElementById("box").onclick=function() {
 		clickedTime = Date.now();
 		reactionTime = (clickedTime - newBoxTime)/1000;
+		document.getElementById("reactionTime").innerHTML=reactionTime;
 		this.style.display="none";
 		newBox();
 	};
